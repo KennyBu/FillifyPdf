264e462b52c16cb2432f5cf1b429058f57391ac8
Ken Burkhardt <ken.burkhardt@gmail.com>
2014-06-19 15:48:06 -0400

    Latest

--- FillifyPdfConsole/Program.cs
+++ FillifyPdfConsole/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
+using iTextSharp.text.pdf;
+using Color = iTextSharp.text.Color;
+using Font = iTextSharp.text.Font;
 
 namespace FillifyPdfConsole
 {
@@ -9,8 +11,46 @@ namespace FillifyPdfConsole
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello World!");
+            const string fileBase = "Docs/MerchandiseReturnLabelInstructions";
+            const string fileExtension = ".pdf";
+            
+            var pdfTemplate = string.Concat(AppDomain.CurrentDomain.BaseDirectory, fileBase, fileExtension);
+            var newFile = string.Concat(AppDomain.CurrentDomain.BaseDirectory, fileBase, Guid.NewGuid().ToString("N"),fileExtension);
+
+            //ParseTheForm(pdfTemplate);
+            WriteToFormFields(pdfTemplate, newFile);
+
+            Console.Write("Finished!");
             Console.Read();
+        }
+
+        private static void ParseTheForm(string source)
+        {
+            var pdfReader = new PdfReader(source);
+            var fields = pdfReader.AcroFields;
+
+            var sb = new StringBuilder();
+            foreach (var de in pdfReader.AcroFields.Fields.Keys)
+            {
+                sb.Append(de + Environment.NewLine);
+            }
+        }
+
+        private static void WriteToFormFields(string source, string destination)
+        {
+            var bfTimes = BaseFont.CreateFont(BaseFont.TIMES_ROMAN, BaseFont.CP1252, false);
+            var times = new Font(bfTimes, 12, Font.ITALIC, Color.BLACK);
+
+            var pdfReader = new PdfReader(source);
+            pdfReader.RemoveUsageRights();
+            var pdfStamper = new PdfStamper(pdfReader, new FileStream(destination, FileMode.Create));
+            AcroFields fields = pdfStamper.AcroFields;
+            
+            fields.SetFieldProperty("ShipName","textfont", times.BaseFont,null);
+            fields.SetField("ShipName", "Luke Skywalker");
+
+            // close the pdf
+            pdfStamper.Close();
         }
     }
 }
